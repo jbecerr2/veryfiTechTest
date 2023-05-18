a6190fd35f55f1173ee503c2c7cb09bb4d6200eb
Juan Manuel <j_mbc95@hotmail.com>
2023-05-18 16:54:25 -0500

    created a function to check the files in folder

--- docReader.py
+++ docReader.py
@@ -26,7 +26,34 @@ veryfi_client = Client(client_id=client_id, client_secret=client_secret, usernam
 
 #---------------------------------------------------------------------------------------------------------------
 
+def check_files_in_folder(folder_name):
+    # Get the directory path
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-#documents folder path
-docs_path = "/home/jmb/Documents/Projects/Tech_tests/veryfiTechTest/docs/"
+    #folder path
+    folder_path = os.path.join(script_dir, folder_name)
+
+    # Check if the folder exists
+    if not os.path.exists(folder_path):
+        print(f"Folder '{folder_name}' does not exist.")
+        return
+
+    # Get the list of files in the folder
+    files = os.listdir(folder_path)
+
+    # Check if there are files in the folder
+    if len(files) == 0:
+        print(f"No files found in the folder '{folder_name}'.")
+    else:
+        print(f"Files in the folder '{folder_name}':")
+        for file_name in files:
+            file_path = os.path.join(folder_path, file_name)
+            print(file_path)
+
+# Specify the folder name
+folder_name = "docs"
+
+# Check if there are files in the folder
+check_files_in_folder(folder_name)
+
 
