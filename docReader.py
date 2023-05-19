1fa0a1651c4277a9079cb7eaa561f642e7b3fc66
Juan Manuel <j_mbc95@hotmail.com>
2023-05-19 16:58:42 -0500

    Project completed, tested with the provided docs Next step is testing with other and more docs

--- docReader.py
+++ docReader.py
@@ -2,70 +2,74 @@ import os
 import json
 from veryfi import Client
 from dotenv import load_dotenv
-#from pathlib import Path
 
-#Load veryfi API credentials from envfile
-credentials = "envfile.env"
-load_dotenv(credentials)
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-client_id = os.getenv("VERYFI_CLIENT_ID")
-client_secret = os.getenv("VERYFI_CLIENT_SECRET")
-username = os.getenv("VERYFI_USERNAME")
-api_key = os.getenv("VERYFI_API_KEY")
+#Function to load veryfi API credentials from envfile
+def veryfi_credentials():
+    credentials = "envfile.env"
+    load_dotenv(credentials)
 
-#Check if credentials are loaded correctly
-if not all([client_id, client_secret, username, api_key]):
-    print("Error: Failed to load veryfi API credentials, please check envfile.env and verify your credentials")
-    exit()
-else:
-    print("Credentials loaded successfully")
+    client_id = os.getenv("VERYFI_CLIENT_ID")
+    client_secret = os.getenv("VERYFI_CLIENT_SECRET")
+    username = os.getenv("VERYFI_USERNAME")
+    api_key = os.getenv("VERYFI_API_KEY")
+
+    #Check if the credentials are properly loaded from the env file
+    if not all([client_id, client_secret, username, api_key]):
+        raise ValueError("Error: Failed to load Veryfi API credentials, please check the .env file")
+    print("Veryfi credentials loaded successfully")
     
-#Create client with the veryfi credentials
-veryfi_client = Client(client_id=client_id, client_secret=client_secret, username=username, api_key=api_key)
+    return client_id, client_secret, username, api_key
 
+
+#Function to extract the specific information from a document
 def extract_information(response):
-    extracted_data = {}
+    extracted_data = {
+        'vendor_name':response['vendor']['name'],
+        'bill_to_name':response['bill_to']['name'],
+        'bill_to_address':response['bill_to']['address'],
+        'ship_to_name':response['ship_to']['name'],
+        'ship_to_address':response['ship_to']['address'],
+        'line_items':[
+            {
+                'quantity':line_item['quantity'],
+                'description':line_item['description'],
+                'price': line_item['total']
+            }
+            for line_item in response['line_items']
+        ]
 
-    # Extract vendor name
-    extracted_data['vendor_name'] = response['vendor']['name']
-
-    # Extract bill to information
-    bill_to = response['bill_to']
-    extracted_data['bill_to_name'] = bill_to['name']
-    extracted_data['bill_to_address'] = bill_to['address']
-
-    # Extract ship to information
-    ship_to = response['ship_to']
-    extracted_data['ship_to_name'] = ship_to['name']
-    extracted_data['ship_to_address'] = ship_to['address']
-
-    # Extract line items
-    line_items = response['line_items']
-    extracted_line_items = []
-    for line_item in line_items:
-        extracted_line_item = {
-            'quantity': line_item['quantity'],
-            'description': line_item['description'],
-            'price': line_item['total']
-        }
-        extracted_line_items.append(extracted_line_item)
-
-    extracted_data['line_items'] = extracted_line_items
-
+    }  
     return extracted_data
 
-# Specify the file path of the document
-file_path = "docs/0001431487.jpg"
+#Function to read all the documents present in a directory
+def process_documents(directory_path, client):
+    output_data = {"documents":[]}
 
-# Process the document using Veryfi API
-response = veryfi_client.process_document(file_path)
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        response = client.process_document(file_path)
+        extracted_info = extract_information(response)
+        output_data["documents"].append(extracted_info)
+    return output_data
 
-# Extract the desired information
-extracted_information = extract_information(response)
+#Function to save all the processed data into a json file
+def save_to_json(data,output_path):
+    with open(output_path,"w") as outfile:
+        json.dump(data, outfile, indent=4)
 
-# Create a dictionary to store the extracted data
-output_data = {"documents": [extracted_information]}
 
-# Save the extracted information to a JSON file
-with open("output.json", "w") as outfile:
-    json.dump(output_data, outfile, indent=4)
+def main():
+    
+    client_id, client_secret, username, api_key = veryfi_credentials()
+    veryfi_client = Client(client_id=client_id, client_secret=client_secret, username=username, api_key=api_key)
+
+    directory_path="docs/"
+    output_path="output.json"
+
+    output_data = process_documents(directory_path, veryfi_client)
+    save_to_json(output_data, output_path)
+
+if __name__=="__main__":
+    main()
